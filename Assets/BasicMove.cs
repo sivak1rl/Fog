0b9b2619b0d789907d2ede25513d80d02ce90d2c
Richard Sivak <sivak.r101@gmail.com>
2017-01-22 15:00:10 -0500

    Fog game for Global Game Jam. Also, init

---
+++ Assets/BasicMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This is made to move a camera around the screen. I basically ignore that physics are a thing
+/// </summary>
+public class BasicMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var heightChange = Input.GetKey(KeyCode.LeftShift) ? Time.deltaTime * -moveSpeed : Input.GetKey(KeyCode.Space) ? Time.deltaTime * moveSpeed : 0;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, heightChange, z);
+    }
+}
